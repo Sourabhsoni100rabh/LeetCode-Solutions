b08dac760a4175e29d039bbfe292fc1cdee2ced5
Sourabh soni <sourabhsoniak47@gmail.com>
2026-04-11 23:29:13 +0530

    Update 1588. Sum of All Odd Length Subarrays.java

--- Arrays/1588. Sum of All Odd Length Subarrays.java
+++ Arrays/1588. Sum of All Odd Length Subarrays.java
@@ -1,1 +1,18 @@
+//problem: sum of all odd length sub array
+// leetcode: https://leetcode.com/problems/sum-of-all-odd-length-subarrays/submissions/1975665418/?envType=problem-list-v2&envId=prefix-sum
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+    int n = arr.length;
+        int sum = 0;
 
+        for (int i = 0; i < n; i++) {
+            int totalSubarrays = (i + 1) * (n - i);
+            int oddSubarrays = (totalSubarrays + 1) / 2;
+            sum += arr[i] * oddSubarrays;
+        }
+
+        return sum;
+    }
+}
